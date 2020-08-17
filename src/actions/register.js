5b85ee3fe9a144d9e2ddf59eb9911fb439b220ca
datagirl <datagirlcmr@gmail.com>
2020-08-17 07:34:14 +0100

    connect register page to api

--- src/actions/register.js
+++ src/actions/register.js
@@ -1,30 +1,32 @@
-import { loginUser, STEPS_API } from './index';
+import { LOGIN_USER, BASE_URL } from "./index";
+import { inputValidation, loadingIcon } from "../helper/index";
 
 function createUser(data) {
-    return dispatch => {
-        const event = new FormData();
-        for (const name in data) {
-            event.append(name, data[name]);
+  return (dispatch) => {
+  
+    fetch(`${BASE_URL}/users`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(data),
+      })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.error) {
+          throw res.error;
         }
-        fetch(`${STEPS_API}/users`,
-            {
-                method: 'POST',
-                body: event,
-            })
-            .then(res => res.json())
-            .then(res => {
-                if (res.error) {
-                    throw (res.error);
-                }
-                if (res.auth_token !== undefined) {
-                    dispatch(loginUser(res));
-                    //   } else {
-                    //     inputValidation(res);
-                }
-                return res;
-            })
-            .catch(error => error);
-    };
+        if (res.auth_token !== undefined) {
+          dispatch(LOGIN_USER(res));
+        } else {
+          loadingIcon();
+          inputValidation(res);
+        }
+        return res;
+      })
+      .catch((error) => error);
+  };
 }
 
 export default createUser;
--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,75 +1,131 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import webimg from '../assets/feet.jpg'
+import webimg from "../assets/feet.jpg";
+import { connect } from "react-redux";
+import createUser from "../actions/register";
+import fetchUser from "../actions/fetchUserDetails";
+import { loadingIcon } from "../helper/index";
 
 class Register extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      password_confirmation: "",
+      image: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-        this.state = {
-            name: "",
-            email: "",
-            password: "",
-            password_confirmation: "",
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.createUser(this.state);
+    const { store } = this.props;
+    if (store.user.auth_token !== "") {
+      this.setState({
+        name: "",
+        email: "",
+        password: "",
+        password_confirmation: "",
+      });
     }
+  }
 
-    handleChange(event) {
-        const newState = event.target.value;
-        const prevState = this.state;
-        this.setState({ ...prevState, [event.target.name]: newState });
-    }
+  handleChange(event) {
+    const newState = event.target.value;
+    const prevState = this.state;
+    this.setState({ ...prevState, [event.target.name]: newState });
+  }
 
-    handleSubmit(event) {
-        console.log("submitted");
-    }
+  render() {
+    const { name, email, password, password_confirmation } = this.state;
+    return (
+      <div className="wrapper" style={{ backgroundImage: `url(${webimg})` }}>
+        <div className="inner">
+          <div className="image-holder">
+            <img src={webimg} alt="" />
+          </div>
+          <form onSubmit={this.handleSubmit}>
+            <h3>Registration Form</h3>
 
-    render() {
-        const { name, email, password, password_confirmation } = this.state;
-        return (
-            <div className="wrapper" style={{ backgroundImage: `url(${webimg})` }} >
-                <div className="inner">
-                    <div className="image-holder">
-                        <img src={webimg} alt="" />
-                    </div>
-                    <form >
-                        <h3>Registration Form</h3>
-
-                        <div className="form-group">
-                            <input type="text" required name="name" value={name} placeholder="Name" onChange={this.handleChange} className="form-control" />
-                        </div>
-
-                        <div className="form-wrapper">
-                            <input type="email" required name="email" value={email} placeholder="Email Address" onChange={this.handleChange} className="form-control" />
-                            <i className="zmdi zmdi-email"></i>
-                        </div>
-
-                        <div className="form-wrapper">
-                            <input type="password" required name="password" value={password} placeholder="Password" onChange={this.handleChange} className="form-control" />
-                            <i className="zmdi zmdi-lock"></i>
-                        </div>
-
-                        <div className="form-wrapper">
-                            <input type="password" required name="password_confirmation" value={password_confirmation} placeholder="Password Confirmation" onChange={this.handleChange} className="form-control" />
-                            <i className="zmdi zmdi-lock"></i>
-                        </div>
-
-                        <button>Register
-						<i className="zmdi zmdi-arrow-right"></i>
-                        </button>
-
-                    </form>
-                    <div>
-                        <p className="form-wrapper"> If you already have an account , Login <Link to="/login">Here</Link></p>
-                    </div>
-                </div>
-
+            <div className="form-group">
+              <input
+                type="text"
+                required
+                name="name"
+                value={name}
+                placeholder="Name"
+                onChange={this.handleChange}
+                className="form-control"
+              />
             </div>
-        );
-    }
+
+            <div className="form-wrapper">
+              <input
+                type="email"
+                required
+                name="email"
+                value={email}
+                placeholder="Email Address"
+                onChange={this.handleChange}
+                className="form-control"
+              />
+              <i className="zmdi zmdi-email"></i>
+            </div>
+
+            <div className="form-wrapper">
+              <input
+                type="password"
+                required
+                name="password"
+                value={password}
+                placeholder="Password"
+                onChange={this.handleChange}
+                className="form-control"
+              />
+              <i className="zmdi zmdi-lock"></i>
+            </div>
+
+            <div className="form-wrapper">
+              <input
+                type="password"
+                required
+                name="password_confirmation"
+                value={password_confirmation}
+                placeholder="Password Confirmation"
+                onChange={this.handleChange}
+                className="form-control"
+              />
+              <i className="zmdi zmdi-lock"></i>
+            </div>
+
+            <button>
+              Register
+              <i className="zmdi zmdi-arrow-right"></i>
+            </button>
+          </form>
+          <div>
+            <p className="form-wrapper">
+              {" "}
+              If you already have an account , Login{" "}
+              <Link to="/login">Here</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Register;
+
+const mapDispatchToProps = {
+  createUser,
+  fetchUser,
+};
+
+const mapStateToProps = store => ({ store });
+
+export default connect(mapStateToProps, mapDispatchToProps)(Register);
